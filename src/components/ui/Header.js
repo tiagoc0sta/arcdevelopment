4866a9e69c0485c59783c79e7e060e7d2ddd0f4a
Tiago Costa <tiagodpv@gmail.com>
2023-02-07 19:44:45 -0500

    add header

---
+++ src/components/ui/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+export default function Header(props) {
+  return (
+    <AppBar position='fixed'>
+      <Toolbar>Arc Development</Toolbar>
+    </AppBar>
+  );
+}
