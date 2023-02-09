78dc536f31ef62be0d8d4fca38ae591aebcf8981
Tiago Costa <tiagodpv@gmail.com>
2023-02-08 19:20:46 -0500

    add styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/ui/Header';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 function App() {
   return (
     <div className='App'>
-      <Header /> Hello!
+      <Header />
+      Hello
     </div>
   );
 }
